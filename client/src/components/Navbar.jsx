b5cb0fa1b4683c89ed91e794ac837a44a3c41da2
aditya <mp7020216@gmail.com>
2023-09-16 01:38:26 +0530

    Add code for building Sidebar using material UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -19,9 +19,10 @@ import {
   useTheme,
 } from "@mui/material";
 
-const Navbar = () => {
+const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch();
   const theme = useTheme();
+
   return (
     <AppBar
       sx={{
@@ -33,7 +34,7 @@ const Navbar = () => {
       <Toolbar sx={{ justifyContent: "space-between" }}>
         {/* LEFT SIDE */}
         <FlexBetween>
-          <IconButton onClick={() => console.log("open/close sidebar")}>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
             <MenuIcon />
           </IconButton>
           <FlexBetween
